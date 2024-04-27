84daab468a78c566a3904db912e63bbc564db4f0
Chris Tate <ctate752@gmail.com>
2024-04-27 18:48:39 -0400

    Create PhotoObjects.tsx

---
+++ components/PhotoObjects.tsx
@@ -0,0 +1,284 @@
+export const WeddingPhotos = [
+  {
+    src: "@/public/Photos/Anemone_Candles.webp",
+    alt: "anemone candles",
+  },
+  {
+    src: "@/public/Photos/Weddings/Anemone_Waxflower.webp",
+    alt: "anemone waxflower",
+  },
+  {
+    src: "@/public/Photos/Weddings/Babys_Breath_Mums.webp",
+    alt: "baby breath mums",
+  },
+  {
+    src: "@/public/Photos/Weddings/Bridal_Bouquet.webp",
+    alt: "bridal bouquet",
+  },
+  {
+    src: "@/public/Photos/Weddings/Bride_Bouquet.webp",
+    alt: "bride bouquet",
+  },
+  {
+    src: "@/public/Photos/Weddings/Bridesmaids_Bouquets.webp",
+    alt: "brudesmaids bouquet",
+  },
+  {
+    src: "@/public/Photos/Weddings/Budvase_Arrangement.webp",
+    alt: "bud vase arrangements",
+  },
+  {
+    src: "@/public/Photos/Weddings/Candle_Centerpiece.webp",
+    alt: "candle center piece",
+  },
+  {
+    src: "@/public/Photos/Weddings/Gerberdaisy_Mum.webp",
+    alt: "gerberdaisy mums",
+  },
+  {
+    src: "@/public/Photos/Weddings/Hydrangea_Bar_Arrangement.webp",
+    alt: "hydrangea bar arrangement",
+  },
+  {
+    src: "@/public/Photos/Weddings/Large_Ceremony_Arrangement.webp",
+    alt: "large ceremony arrangement",
+  },
+  {
+    src: "@/public/Photos/Weddings/Large_Wedding_Arrangement.webp",
+    alt: "large wedding arrangement",
+  },
+  {
+    src: "@/public/Photos/Weddings/Outdoor_Wedding_Decor.webp",
+    alt: "outdoor wedding decor",
+  },
+  {
+    src: "@/public/Photos/Weddings/Rose_Boutonniere.webp",
+    alt: "rose boutonniere",
+  },
+  {
+    src: "@/public/Photos/Weddings/Rose_Queenannes_Lace.webp",
+    alt: "rose queenannes",
+  },
+  {
+    src: "@/public/Photos/Weddings/Small_Arrangement.webp",
+    alt: "small arrangements",
+  },
+  {
+    src: "@/public/Photos/Weddings/Sprayroses_Arrangement.webp",
+    alt: "spray roses arrangements",
+  },
+  {
+    src: "@/public/Photos/Weddings/Sweetheart_Table.webp",
+    alt: "sweetheart table arrangement",
+  },
+  {
+    src: "@/public/Photos/Weddings/Table_Decor.webp",
+    alt: "table decor",
+  },
+  {
+    src: "@/public/Photos/Weddings/Trellice_Decor.webp",
+    alt: "trellice decor",
+  },
+  {
+    src: "@/public/Photos/Weddings/Trellice_Flowers.webp",
+    alt: "trellice flowers",
+  },
+  {
+    src: "@/public/Photos/Wrist_Coursage.webp",
+    alt: "wrist coursage",
+  },
+];
+
+export const SpecialOccassionPhotos = [
+  {
+    src: "@/public/Photos/SpecialOccassions/Autumn_Centerpiece.webp",
+    alt: "autumn center piece",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/Autumn_Wreath.webp",
+    alt: "autumn wreaths",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/BabyShower_Arrangement.webp",
+    alt: "baby shower arrangement",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/BabyShower_Table_Runner.webp",
+    alt: "baby shower table runner",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/BabyShower_Table_Runner.webp",
+    alt: "baby shower table runner",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/Birthday_Budvases.webp",
+    alt: "birthday bud vases",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/Birthday_Centerpiece.webp",
+    alt: "birthday centerpiece",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/Birthday_Piece.webp",
+    alt: "birthday piece",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/Bottle_Arrangement.webp",
+    alt: "bottle arrangements",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/Bottle_Piece.webp",
+    alt: "bottle arrangement",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/Candle_Centerpiece.webp",
+    alt: "candle centerpiece",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/Candycane_Wreath.webp",
+    alt: "candy cane wreath",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/Casket_Spray.webp",
+    alt: "casket spray",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/Custom_Birthday_Arrangement.webp",
+    alt: "custom birthday arrangement",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/Custom_Fall_Decor.webp",
+    alt: "custom autumn decor",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/Custom_Moon_Arrangement.webp",
+    alt: "custom moon-shaped decor",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/Custom_Umbrella_Arrangement.webp",
+    alt: "custom umbrella-hung decor",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/Fall_Wreath.webp",
+    alt: "fall wreath",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/Front_Door_Wreath.webp",
+    alt: "general front door wreath",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/Funeral_Spray.webp",
+    alt: "funeral spray",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/Funeral_Wreath.webp",
+    alt: "funeral wreath",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/Holiday_Wreath.webp",
+    alt: "holiday wreath",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/July_Arrangement.webp",
+    alt: "fourth of july arrangement",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/Large_Party_Arrangement.webp",
+    alt: "large party arrangement",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/LemonThemed_Arrangement.webp",
+    alt: "lemon themed arrangement",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/Orchard_Spray.webp",
+    alt: "orchard spray",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/Roses_Wreath.webp",
+    alt: "roses wreath",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/Sasket_Spray.webp",
+    alt: "large casket spray",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/Spring_Wreath.webp",
+    alt: "spring wreath",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/Table_Runner.webp",
+    alt: "general table runner",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/Warm_Rose_Wreath.webp",
+    alt: "warm rose wreath",
+  },
+  {
+    src: "@/public/Photos/SpecialOccassions/White_Holiday_Wreath.webp",
+    alt: "white holiday wreath",
+  },
+];
+
+export const EverydayPhotos = [
+  {
+    src: "@/public/Photos/Everyday/Birthday_Bouquet.webp",
+    alt: "birthday bouquet",
+  },
+  {
+    src: "@/public/Photos/Everyday/Celebration_Bouquet.webp",
+    alt: "celebration bouquet",
+  },
+  {
+    src: "@/public/Photos/Everyday/Custom_Easter.webp",
+    alt: "custom easter arrangement",
+  },
+  {
+    src: "@/public/Photos/Everyday/Easter_Arrangement.webp",
+    alt: "easter arrangement",
+  },
+  {
+    src: "@/public/Photos/Everyday/Entry_Arrangement.webp",
+    alt: "entry arrangement",
+  },
+  {
+    src: "@/public/Photos/Everyday/Iris_Arrangement.webp",
+    alt: "iris arrangement",
+  },
+  {
+    src: "@/public/Photos/Everyday/Memory_Arrangement.webp",
+    alt: "memory arrangement",
+  },
+  {
+    src: "@/public/Photos/Everyday/Mothers_Day.webp",
+    alt: "mothers day arrangement",
+  },
+  {
+    src: "@/public/Photos/Everyday/Reception_Arrangement.webp",
+    alt: "reception arrangement",
+  },
+  {
+    src: "@/public/Photos/Everyday/Rose_Assorted.webp",
+    alt: "rose arrangement",
+  },
+  {
+    src: "@/public/Photos/Everyday/St_Patricks_Arrangement.webp",
+    alt: "st patricks arrangement",
+  },
+  {
+    src: "@/public/Photos/Everyday/Valentine_Bouquet.webp",
+    alt: "valentines bouquet",
+  },
+  {
+    src: "@/public/Photos/Everyday/Warm_Arrangement.webp",
+    alt: "warm arrangements",
+  },
+  {
+    src: "@/public/Photos/Everyday/WelcomeTable_Arrangement.webp",
+    alt: "welcome table arrangements",
+  },
+  {
+    src: "@/public/Photos/Everyday/XL_Valentine.webp",
+    alt: "extra large valentine arrangement",
+  },
+];
